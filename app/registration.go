98ca5e66cb736971700ac14ea8a14615eeda15e6
SvitlanaKyrychenko <svitlana.kyrychenko@ukma.edu.ua>
2021-08-03 10:28:05 +0300

    logic of sign up moved to a separate method called trySignUp

--- app/registration.go
+++ app/registration.go
@@ -24,18 +24,10 @@ func RegistrationHandler(response http.ResponseWriter, request *http.Request) {
 		signUp := request.FormValue("signUp")
 
 		if signUp == "Sign up" {
-			if registrationUser.Email == "" || registrationUser.Password == "" || registrationUser.ConfirmPassword == "" {
-				viewData = ViewData{"Email, password and confirm password must be set"}
-			} else if validEmail(registrationUser.Email) {
-				viewData = ViewData{"Email os wrong"}
-			} else if len(registrationUser.Password)<6 {
-				viewData = ViewData{"Password must be longer than 6"}
-			}else if registrationUser.Password != registrationUser.ConfirmPassword {
-				viewData = ViewData{"Confirm password and password can`t be different"}
-			}else{
-				if Registration(registrationUser){
-					http.Redirect(response, request, "/user/login", 301)
-				}
+			if valid, message := trySignUp(registrationUser); !valid{
+				viewData = ViewData{message}
+			}else {
+				http.Redirect(response, request, "/user/login", 301)
 			}
 		} else if goToSignIn == "Go to sign in" {
 			http.ServeFile(response, request, "../WebApiGenesis/html/authentication.html")
@@ -44,6 +36,25 @@ func RegistrationHandler(response http.ResponseWriter, request *http.Request) {
 		tmpl, _ := template.ParseFiles("../WebApiGenesis/html/registration.html")
 		tmpl.Execute(response, viewData)
 	}
+}
+
+func trySignUp(registrationUser RegistrationUser) (bool, string) {
+	if registrationUser.Email == "" || registrationUser.Password == "" || registrationUser.ConfirmPassword == "" {
+		return false, "Email, password and confirm password must be set"
+	}
+	if validEmail(registrationUser.Email) {
+		return false, "Email is wrong"
+	}
+	if len(registrationUser.Password) < 6 {
+		return false, "Password must be longer than 6"
+	}
+	if registrationUser.Password != registrationUser.ConfirmPassword {
+		return false, "Confirm password and password can`t be different"
+	}
+	if !Registration(registrationUser) {
+		return false, "This user has not registered yet"
+	}
+	return true, ""
 }
 
 func validEmail(email string) bool {
